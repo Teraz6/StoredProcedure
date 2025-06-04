9e3155246091bf53dab9ecafb5e9864825940c6b
Teraz6 <retenk@gmail.com>
2025-06-04 11:02:37 +0300

    Init

---
+++ StoredProcedures/StoredProcedures/Data/StoredProcDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoredProcedures.Models;
+
+namespace StoredProcedures.Data
+{
+    public class StoredProcDbContext : DbContext
+    {
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext> options)
+            : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ StoredProcedures/StoredProcedures/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoredProcedures.Models
+{
+    public class Employee
+    {
+        [Key]
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Gender { get; set; } = string.Empty;
+
+        public int Salary { get; set; }
+    }
+}
