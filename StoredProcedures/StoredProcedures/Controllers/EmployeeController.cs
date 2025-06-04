4e456753a52bd3de378e672a90974dca5da5c07b
Teraz6 <retenk@gmail.com>
2025-06-04 11:24:48 +0300

    Add connetion to SQL and Employee index view

---
+++ StoredProcedures/StoredProcedures/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoredProcedures.Data;
+using StoredProcedures.Models;
+
+namespace StoredProcedures.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public StoredProcDbContext _context;
+        public IConfiguration _confiq {  get; } 
+
+        public EmployeeController
+            (
+                StoredProcDbContext context,
+                IConfiguration confiq 
+            )
+        {
+            _context = context;
+            _confiq = confiq;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //teha sp andmebaasi, mis annab andmed töötajatest
+        public IEnumerable<Employee> SearchResult()
+        {
+            var result = _context.Employees
+                .FromSqlRaw<Employee>("spSearchEmployees")
+                .ToList();
+
+            return result;
+        }
+    }
+}
--- StoredProcedures/StoredProcedures/Program.cs
+++ StoredProcedures/StoredProcedures/Program.cs
@@ -1,7 +1,14 @@
+using StoredProcedures.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<StoredProcDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
